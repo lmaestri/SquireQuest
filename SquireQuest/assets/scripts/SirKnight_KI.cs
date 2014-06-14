1d8d8226011d73f972e2a97fb7155132ee42cd28
lmaestri <luca.maestri@hotmail.de>
2014-06-14 16:26:10 +0200

    start project
    base project to work on

---
+++ SquireQuest/assets/scripts/SirKnight_KI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SirKnight_KI : MonoBehaviour {
+
+	float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody2D.velocity = new Vector2(speed, 0.0f);
+	}
+}
---
+++ SquireQuest/assets/scripts/SquireControl.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+public class SquireControl : MonoBehaviour {
+
+	float maxSpeed = 10.0f;
+
+
+	bool facingRight = true;
+
+	bool grounded = true;
+
+	float jumpSpeed = 3000.0f;
+
+	public GameObject bridge;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float move = Input.GetAxis ("Horizontal");
+
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+		if (move > 0 && !facingRight)
+				Flip ();
+		else if (move < 0 && facingRight)
+				Flip ();
+
+		float jump = Input.GetAxis ("Vertical");
+		if( jump > 0 && grounded){
+			rigidbody2D.AddForce( transform.up * jumpSpeed );
+			grounded = false;
+		}
+
+	}
+
+	void Flip() {
+
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		string input = collision.collider.name;
+		Match match1 = Regex.Match(input,"Dirt*");
+		Match match2 = Regex.Match(input,"Stone*");
+		//if(collision.collider.transform.parent.name.Equals("Level");
+		if(!grounded && match1.Success){
+			grounded = true;
+		}
+
+		if( input.Equals("Bridge") ){
+			Instantiate(bridge, new Vector3(51.0f, 4.0f, 0.0f), new Quaternion());
+		}
+
+		if( match2.Success){
+			Destroy(GameObject.Find("ObstacleRock01"));
+		}
+
+
+
+	}
+}
