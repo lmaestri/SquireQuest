52f9455d023d2d57acd5b3a21a15693a8279e7a1
ZenoLC <zeno.purple@gmail.com>
2014-06-15 18:46:19 +0200

    almost final

--- SquireQuest/assets/scripts/SirKnightTest.cs
+++ SquireQuest/assets/scripts/SirKnightTest.cs
@@ -1,18 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SirKnight_KI : MonoBehaviour {
+public class SirKnightTest : MonoBehaviour {
 
 	public float speed = 10.0f;
 
 	// Use this for initialization
 	void Start () {
-
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-		if (transform.position.x < 682.74)
-			rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+	void Update () {
+		
+		rigidbody2D.velocity = new Vector2 (speed, rigidbody2D.velocity.y);
 	}
 }
