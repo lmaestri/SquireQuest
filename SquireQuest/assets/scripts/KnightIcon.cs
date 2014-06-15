3aef037a366738b42fd8893d6cc598c52bd090a4
lmaestri <luca.maestri@hotmail.de>
2014-06-15 17:16:23 +0200

    Added KnightIcon
    Added an icon showing the knights position relative to the player

---
+++ SquireQuest/assets/scripts/KnightIcon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnightIcon : MonoBehaviour {
+
+	public GameObject knight;
+	public GameObject player;
+	public float icon_distance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 kpos = knight.transform.position;
+		Vector3 ppos = player.transform.position;
+
+		Vector3 iconpos = kpos - ppos;
+		iconpos.Normalize();
+
+		transform.position = player.transform.position + iconpos * icon_distance;
+
+	}
+}
