cb299d2c15a7760929df1bdb2fd5d2b4dc19c23c
ZenoLC <zeno.casellato@gmail.com>
2014-06-20 16:56:13 +0200

    catapult works
    added horizontal force to knight

--- SquireQuest/assets/scripts/SirKnight_KI.cs
+++ SquireQuest/assets/scripts/SirKnight_KI.cs
@@ -6,7 +6,7 @@ public class SirKnight_KI : MonoBehaviour {
 
 	public float speed = 10.0f;
 	public GameObject rock;
-	public GameObject sign;
+	public GameObject wood;
 
 	bool facingRight = true;
 
@@ -18,14 +18,27 @@ public class SirKnight_KI : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		Vector2 pos = rigidbody2D.position;
-		if (pos.x < 682.74)
-			rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+		if (pos.x < 665)
+			if (speed > rigidbody2D.velocity.x)
+				rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+			else
+				rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, rigidbody2D.velocity.y);
+		//resets on death
 		if (pos.x > 282 && pos.y > (-80) && pos.y < (-65))
 			rigidbody2D.position = new Vector2 (146f, -24f);
 		if (pos.x < 282 && pos.y < (-233))
 			rigidbody2D.position = new Vector2 (93f, -115f);
-		if (pos.x > 282 && pos.y < (-233))
+		if (pos.x > 282 && pos.y < (-233)) {
 			rigidbody2D.position = new Vector2 (298f, -205f);
+			rock.rigidbody2D.position = new Vector2(526.1115f, -88.02936f);
+			rock.rigidbody2D.velocity = new Vector2(0.0f, 0.0f);
+			rock.rigidbody2D.angularVelocity = 0.0f;
+			rock.rigidbody2D.rotation = 0.0f;
+			wood.rigidbody2D.position = new Vector2(528.4794f, -210.829f);
+			wood.rigidbody2D.velocity = new Vector2(0.0f, 0.0f);
+			wood.rigidbody2D.angularVelocity = 0.0f;
+			wood.rigidbody2D.rotation = 0.0f;
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
